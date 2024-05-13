129996399680e7c2110940a0e3a6f3926d88bc4f
gabriel <liviu.gg@gmail.com>
2024-05-13 21:14:50 +0300

    tema3

---
+++ tema3.js
@@ -0,0 +1,11 @@
+let myArray = [ 10, 11, 12, 13, 14, 15 ]
+
+let reversed = myArray.reverse (function ( a, b) {
+    return b, a;   
+})
+
+console.log(reversed);
+
+
+// let myArray = [ 10, 11, 12, 13, 14, 15 ]
+// console.log(myArray.reverse())
